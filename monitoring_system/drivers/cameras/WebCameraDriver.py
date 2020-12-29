b37b870b5d0c6c65d944d5b3033a55b249e13a56
sergey <sergei.nesteruk@skoltech.ru>
2020-12-29 14:20:58 +0300

    Add programmatic exposure

--- monitoring_system/drivers/cameras/WebCameraDriver.py
+++ monitoring_system/drivers/cameras/WebCameraDriver.py
@@ -1,5 +1,6 @@
 import cv2
 import time
+import numpy as np
 
 from monitoring_system.drivers.cameras.CameraDriver import CameraDriver
 
@@ -23,20 +24,51 @@ class WebCameraDriver(CameraDriver):
         self.cam.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'))
 
         # Take some time for camera initialization
+        _, _ = self.cam.read()
         time.sleep(0.1)
 
     def capture(self):
         for i in range(self.camera_info['focus_skip'] + 1):
-            time.sleep(0.1)
-            try:
-                ret, self.captured_image = self.cam.read()
-                if not ret:
-                    self.captured_image = None
-            except Exception as e:
-                self._send_alert(e)
+            ret, image = self.cam.read()
+
+        if self.camera_info['average'] > 0:
+            images = []
+            for i in range(self.camera_info['average']):
+                image = self.capture_single()
+
+                if image is not None:
+                    images.append(image)
+
+            if len(images) < 1:
+                self._send_alert('Not enough images to average')
+                self.captured_image = image
+            else:
+                images = np.array(images)
+                num_images = images.shape[0]
+                res = (images.sum(axis=0) / num_images).astype('uint8')
+                self.captured_image = res
+
+
+        else:
+            self.captured_image = image
 
         self.cam.release()
         cv2.destroyAllWindows()
         del self.cam
 
         self._save_all()
+
+    def capture_single(self):
+
+        time.sleep(0.1)
+
+        try:
+            ret, captured_image = self.cam.read()
+            if not ret:
+                captured_image = None
+
+            return captured_image
+
+        except Exception as e:
+            self._send_alert(e)
+            return None
